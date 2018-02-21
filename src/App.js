4b097b962094f4200e12e6a59aa88ff3f3b492a9
Billi Ramirez <ramirezbilly42@outlook.com>
2018-02-21 17:19:45 -0600

    outputting lists

--- src/App.js
+++ src/App.js
@@ -54,17 +54,14 @@ class App extends Component {
     if (this.state.showPersons){
       persons = (
         <div>
-          <Person
-            name={this.state.persons[0].name}
-            age={this.state.persons[0].age} />
-          <Person
-            name={this.state.persons[1].name}
-            age={this.state.persons[1].age}
-            click={this.switchNameHandler.bind(this, 'Billi!')}
-            changed={this.nameChangedHandler} >My hobbies: singing </Person>
-          <Person
-            name={this.state.persons[2].name}
-            age={this.state.persons[2].age} />
+        {
+          this.state.persons.map((person, index) => {
+            return <Person
+            key={index} 
+            name={person.name} 
+            age={person.age} />
+          })
+        }
         </div>
       );
     }
