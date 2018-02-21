1056f4c52ccda81297bb44fb1aa344328fd36f1a
Billi Ramirez <ramirezbilly42@outlook.com>
2018-02-21 13:47:48 -0600

    handling event with methods

--- src/App.js
+++ src/App.js
@@ -1,20 +1,33 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Person from './Person/Person';
 
 class App extends Component {
+
+  state = {
+    persons : [
+      {name: 'Max', age: 28},
+      {name: 'Manu', age: 29},
+      {name: 'Stephanie', age: 26}
+    ]
+  }
+
+   switchNameHandler = ()=>{
+    console.log('Was clicked')
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <h1> Hello, I'm a react app  </h1>
+        <p>This is really working</p>
+        <button onClick={this.switchNameHandler}>Switch Name</button>
+        <Person name={this.state.persons[0].name} age={this.state.persons[0].age}/>
+        <Person name={this.state.persons[1].name} age={this.state.persons[1].age}>My hobbies: singing </Person>
+        <Person name={this.state.persons[2].name} age={this.state.persons[2].age}/>
       </div>
     );
+    //   return React.createElement('div', {className: 'App   '}, React.createElement('h1', null, 'does it work?'));
   }
 }
 
