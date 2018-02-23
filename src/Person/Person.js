2c1454bc54ed56ff8c43ee96da20daf9605a455c
Billi Ramirez <ramirezbilly42@outlook.com>
2018-02-23 14:05:25 -0600

    working with media queries with the css modules

--- src/Person/Person.js
+++ src/Person/Person.js
@@ -7,7 +7,7 @@ const person = (props) => {
     
 
     return (
-        <div className={classes.person} >
+        <div className={classes.Person} >
             <p onClick={props.click}>I'm a {props.name} and I am {props.age} years old! </p>
             <p>{props.children}</p>
             <input type="text" onChange={props.changed} value={props.name}/>
