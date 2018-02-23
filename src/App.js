1640ae032f18203f47eb3ecc9a69d98d44fea04d
Billi Ramirez <ramirezbilly42@outlook.com>
2018-02-23 13:39:37 -0600

    enabling and using CSS modules, by the comand rpm run eject

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import './App.css';
+import classes from './App.css';
 import Person from './Person/Person';
 
 
@@ -77,12 +77,12 @@ class App extends Component {
       
     }
 
-    const classes = [];
+    const assignedClasses = [];
     if(this.state.persons.length <= 2){
-        classes.push('red'); //classes will be red
+      assignedClasses.push(classes.red); //classes will be red
     }
     if(this.state.persons.length <= 1 ){
-      classes.push('bold');//classes = ['red', 'bold']
+      assignedClasses.push(classes.bold);//classes = ['red', 'bold']
     }
 
 
@@ -91,9 +91,9 @@ class App extends Component {
 
     return (
    
-      <div className="App">
+      <div className={classes.App}>
         <h1> Hello, I'm a react app  </h1>
-        <p className={classes.join(' ')}>This is really working</p>
+        <p className={assignedClasses.join(' ')}>This is really working</p>
         <button 
         style={style}
         onClick={this.togglePersonsHandler}>Toggle Persons</button>
